df37b7b56bae21ee39cf3028a11f8458091a4ffa
taku-nishi <45715127+taku-nishi@users.noreply.github.com>
2019-01-10 15:31:23 +0900

    Update seo.py

--- seo.py
+++ seo.py
@@ -7,12 +7,9 @@
 from collections import Counter
 from janome.tokenizer import Tokenizer
 import pandas as pd
-import numpy as np
 import bs4
 import re
 import requests
-import lxml.html as lx
-
 
 word_list=[]
 counts=pd.DataFrame()
@@ -40,8 +37,7 @@ def get_word(query):
         if partOfSpeech == "名詞":
             if (partOfSpeech2 != "非自立") and (partOfSpeech2 != "代名詞") and (partOfSpeech2 != "数"):
                 word_list.append(word)
-            
-    word_list
+     
 
     counter = Counter(word_list)
     return counter.most_common()
@@ -49,8 +45,6 @@ def get_word(query):
 #     for count in counter.most_common():
 #         counts=counts.append([list(count)],ignore_index=True)
 #     dataset.append(counts)
-# # print(dataset)
-
 #     return dataset
     
     
